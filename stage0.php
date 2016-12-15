aafe25f84893dbb3bb044317a1c597b682de1817
Pascal Sommer <p@pascalsommer.ch>
2016-12-15 11:56:02 +0100

    Rough structure done

--- stage0.php
+++ stage0.php
@@ -1,25 +1,8 @@
-<?php
-
-$theme = "light.css";
-
-if(isset($_GET['theme'])){
-	$theme = $_GET['theme'];
-}
-
-?>
-
 <html>
 <head>
 	<title>Super awesome website</title>
+	<link rel="stylesheet" type="text/css" href="themes/light.css">
 	<link rel="stylesheet" type="text/css" href="common.css">
-
-	<style>
-		<?php
-	    	include("themes/" . $theme);
-	    ?>
-	</style>
-
-
 </head>
 <body>
 
@@ -29,11 +12,6 @@ if(isset($_GET['theme'])){
 	<span>Contact</span>
 </header>
 
-<div class="flex border-faint round-6">
-	<a class="block c-2 bg-grey round-6" href="?theme=light.css">Light theme</a>
-	<a class="block c-2 bg-grey round-6" href="?theme=dark.css">Dark theme</a>
-</div>
-
 <h1>Some website</h1>
 <p>
 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
@@ -41,10 +19,12 @@ tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
 <br>
 <br>
-
-
+<br>
+<br>
+<p>Plz follow meh on da twittah, etc.</p>
 </body>
 </html>
--- stage2.php
+++ stage2.php
@@ -1,67 +1,45 @@
 <?php
 
-$theme = "light.css";
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-//var_dump($_GET);
+$theme = "light.css";
 
 if(isset($_GET['theme'])){
 	$theme = $_GET['theme'];
 }
 
 if(isset($_POST['name']) && isset($_POST['code'])){
-	$file = fopen("theme/".$_POST['name'] . ".css", 'w');
+	$filename = "themes/" . $_POST['name'] . ".css";
+	echo $filename;
+	$file = fopen($filename, "w");
+	if(! $file){
+		echo "<br>file error";
+	}
 	fwrite($file, $_POST['code']);
 	fclose($file);
-
 }
-
-//echo $theme;
 
 ?>
 
 <html>
 <head>
 	<title>Super awesome website</title>
+	<link rel="stylesheet" type="text/css" href="common.css">
 
-
-<style type="text/css">
-	body{
-		text-align: center;
-		font-family: sans;
-		max-width: 400px;
-		margin: 100px auto;
-	}
-
-	header{
-		position: fixed;
-		top: 0;
-		left: 0;
-		right: 0;
-		
-		height: 60px;
-	}
-
-	span{
-		display: inline-block;
-		margin: 20px 30px;
-	}
-
-	<?php
-	system("ls");
-	?>
-
-	
-</style>
+	<style>
+		<?php
+	    	include("themes/" . $theme);
+	    ?>
+	</style>
 
 </head>
 <body>
 
 <header>
-
-	<span>Home</span>
+	<span><a href="index.html">Home</a></span>
 	<span>About</span>
 	<span>Contact</span>
-
 </header>
 
 <h1>Some website</h1>
@@ -72,13 +50,18 @@ quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
+<br>
+<br>
+<br>
+<hr>
+<h2>Upload your own theme</h2>
+<br>
 <form method="post">
-	<p>name</p>
-<input type="text" name="name"><br>
-<p>code:</p>
-<textarea name="code"></textarea>
-<button></button>>
+	<p>Name</p>
+	<input type="text" name="name"><br>
+	<p>Code</p>
+	<textarea name="code"></textarea><br>
+	<input type="submit" value="Upload theme">
 </form>
 
 </body>
